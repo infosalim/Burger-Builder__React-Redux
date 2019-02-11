141283fa965386ff040526fbe532601b0cfe4c5a
infosalim <salimh5757@gmail.com>
2019-02-11 22:09:12 +0600

    created burger builder component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder/>
       </Layout>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxx from '../../hoc/Auxx';
+import classes from './Layout.css';
 
 const Layout = props => (
     <Auxx>
         <div>Toolbar, Sidebar, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxx>
