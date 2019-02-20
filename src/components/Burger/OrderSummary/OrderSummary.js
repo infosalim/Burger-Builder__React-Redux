1b77ae631f66d7b742393bead1f460450676bcd8
infosalim <salimh5757@gmail.com>
2019-02-21 01:47:23 +0600

    updated some components

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxx from '../../../hoc/Auxx';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // This could be a functional component, doesn't have to be a class
+
+    componentWillUpdate(){
+        console.log('[OrderSummary] will Update');
+    }
+    
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return <li key={igKey}>
                 <span
                     style={{ textTransform: 'capitalize' }}>
                     {igKey}
                 </span>: 
-                {props.ingredients[igKey]}
+                {this.props.ingredients[igKey]}
             </li>
         })
-    return (
-        <Auxx>
+
+        return(
+            <Auxx>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
+            <p>Price: <strong>${this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Auxx>
-    )
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
