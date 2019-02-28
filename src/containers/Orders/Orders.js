d3bdf62ae64dc7dc060bfe9b61de3939ad0407cf
infosalim <salimh5757@gmail.com>
2019-02-28 22:52:08 +0600

    outputing the orders on order page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -14,13 +14,13 @@ class Orders extends Component {
         axios.get('/orders.json')
             .then(res => {
                 const fetchedOrders = [];
-                for(let key in res.data){
+                for (let key in res.data) {
                     fetchedOrders.push({
                         ...res.data[key],
                         id: key
                     });
                 }
-                this.setState({loading: false, orders: fetchedOrders})
+                this.setState({ loading: false, orders: fetchedOrders })
             })
             .catch(err => {
                 this.setState({ loading: false });
@@ -30,8 +30,12 @@ class Orders extends Component {
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         );
     }
