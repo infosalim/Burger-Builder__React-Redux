0d8f8a04cfc02a91952ec5c56641577ab47988b3
infosalim <salimh5757@gmail.com>
2019-02-28 20:55:45 +0600

    order submission passing data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,33 +94,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'SAM',
-        //         address: {
-        //             street: 'Savar',
-        //             zipCode: '1212',
-        //             country: 'Bangladesh'
-        //         },
-        //         email: 'sammemem@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     })
-        //     .catch(err => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
         const queryParams = [];
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: 'checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,27 +2,65 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import axios from '../../../axios-order';
 
 class ContactData extends Component {
     state = {
         name: '',
         email: '',
         address: {
-            street:'',
+            street: '',
             postalCode: ''
-        }
+        },
+        loading: false
     }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.totalPrice,
+            customer: {
+                name: 'MIMI',
+                address: {
+                    street: 'Savar',
+                    zipCode: '1212',
+                    country: 'Bangladesh'
+                },
+                email: 'sammemem@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false});
+                this.props.history.push('/');
+            })
+            .catch(err => {
+                this.setState({ loading: false});
+            });
+    }
+
     render() {
-        return (
-            <div className={classes.ContactData}>
-                <h4>Enter Your Contact Data</h4>
-                <form>
+        let form = (
+            <form>
                     <input className={classes.Input} type='text' name='name' placeholder='Your Name' />
                     <input className={classes.Input} type='email' name='email' placeholder='Your Mail' />
                     <input className={classes.Input} type='text' name='street' placeholder='Street' />
                     <input className={classes.Input} type='number' name='postal' placeholder='Postal Code' />
-                    <Button btnType='Success'>ORDER</Button>
+                    <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
                 </form>
+        );
+
+        if(this.state.loading){
+            form = <Spinner/>
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter Your Contact Data</h4>
+                {form}
             </div>
         );
     }
