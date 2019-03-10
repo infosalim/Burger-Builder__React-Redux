f3e0d0e12438870152830e85d439c1ad91dc91ff
infosalim <salimh5757@gmail.com>
2019-03-11 00:44:45 +0600

    created a custom dynamic input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-order';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -35,27 +36,27 @@ class ContactData extends Component {
         }
         axios.post('/orders.json', order)
             .then(response => {
-                this.setState({ loading: false});
+                this.setState({ loading: false });
                 this.props.history.push('/');
             })
             .catch(err => {
-                this.setState({ loading: false});
+                this.setState({ loading: false });
             });
     }
 
     render() {
         let form = (
             <form>
-                    <input className={classes.Input} type='text' name='name' placeholder='Your Name' />
-                    <input className={classes.Input} type='email' name='email' placeholder='Your Mail' />
-                    <input className={classes.Input} type='text' name='street' placeholder='Street' />
-                    <input className={classes.Input} type='number' name='postal' placeholder='Postal Code' />
-                    <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                <Input inputtype='input' type='text' name='name' placeholder='Your Name' />
+                <Input inputtype='input' type='email' name='email' placeholder='Your Mail' />
+                <Input inputtype='input' type='text' name='street' placeholder='Street' />
+                <Input inputtype='input' type='number' name='postal' placeholder='Postal Code' />
+                <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+            </form>
         );
 
-        if(this.state.loading){
-            form = <Spinner/>
+        if (this.state.loading) {
+            form = <Spinner />
         }
         return (
             <div className={classes.ContactData}>
