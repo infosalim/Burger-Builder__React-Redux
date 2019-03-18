ff7d8e3d0292da998fb4c3e1e6efd0a07d8508ec
infosalim <salimh5757@gmail.com>
2019-03-18 23:27:27 +0600

    worked on the button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,6 @@ class BurgerBuilder extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            purchaseable: false,
             purchasing: false,
             loading: false,
             error: false
@@ -40,7 +39,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({ purchaseable: sum > 0 })
+        return sum > 0.
     }
 
 
@@ -84,7 +83,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemove={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
                         ordered={this.purchaseHandler} />
                 </Auxx>
