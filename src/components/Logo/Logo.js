ff9067559c54a87ee7845530d0d929ff8c8aaa7d
infosalim <salimh5757@gmail.com>
2019-04-04 02:55:06 +0600

    added the authentication

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import burgerLogo from '../../assets/images/logo.png';
 import classes from './Logo.css';
 
 const logo = props => (
-    <div className={classes.Logo}>
-        <img src={burgerLogo} alt="SalBurger" />
-    </div>
+    <Link exact to='/'>
+        <div className={classes.Logo}>
+            <img src={burgerLogo} alt="SalBurger" />
+        </div>
+    </Link>
 );
 
 export default logo;
